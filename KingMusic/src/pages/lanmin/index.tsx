045551da65c4126448a30672526ececf0e9571e4
Miranda719 <984430953@qq.com>
2022-08-01 21:49:57 +0800

    'testtest'

--- KingMusic/src/pages/lanmin/index.tsx
+++ KingMusic/src/pages/lanmin/index.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import styles from './index.less';
+import { AudioOutlined } from '@ant-design/icons';
+import { Input, Space } from 'antd';
+
 
 export default function Page() {
+  const { Search } = Input;
+  const onSearch = (value: any) => console.log(value);
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1890ff',
+      }}
+    />
+  );
+  <Space direction="vertical">
+    <Search
+      placeholder="input search text"
+      onSearch={onSearch}
+      style={{
+        width: 200,
+      }}
+    />
+    <Search
+      placeholder="input search text"
+      allowClear
+      onSearch={onSearch}
+      style={{
+        width: 200,
+      }}
+    />
+    <Search
+      addonBefore="https://"
+      placeholder="input search text"
+      allowClear
+      onSearch={onSearch}
+      style={{
+        width: 304,
+      }}
+    />
+    <Search placeholder="input search text" onSearch={onSearch} enterButton />
+    <Search
+      placeholder="input search text"
+      allowClear
+      enterButton="Search"
+      size="large"
+      onSearch={onSearch}
+    />
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      suffix={suffix}
+      onSearch={onSearch}
+    />
+  </Space>
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>
+        ceshitest
+      </h1>
     </div>
   );
 }
